626b387615cfe4e80a1691f053bcd6b9eb16e692
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2023-04-11 14:13:17 -0600

    Add Chan Vese Segmentation using skimage package  * Add Chan Vese  * Move images to img folder

--- backend/image_processing/noise_removal.py
+++ backend/image_processing/noise_removal.py
@@ -1,7 +1,8 @@
 import cv2
+from skimage import img_as_float
+from skimage.segmentation import chan_vese
 
 def dull_razor(img):
-
     #Gray scale
     gray_scale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY )
 
@@ -34,8 +35,21 @@ def closing(img):
     img = invert_bitwise(img)
     return cv2.morphologyEx(img, cv2.MORPH_OPEN, (3,3))
 
+def opening(img):
+    img = invert_bitwise(img)
+    return cv2.morphologyEx(img, cv2.MORPH_CLOSE, (3,3))
+
 def invert_bitwise(img):
     return cv2.bitwise_not(img)
 
 def and_bitwise(img, msk):
     return cv2.bitwise_and(img, img, mask = msk)
+
+def chan_vese_segmentation(img):
+    image = img_as_float(img)
+    # Feel free to play around with the parameters to see how they impact the result
+    cv = chan_vese(image, mu=0.95, lambda1=1, lambda2=2, tol=1e-3,
+                   max_num_iter=200, dt=0.5, init_level_set="checkerboard",
+                   extended_output=False)
+    return img * cv
+
