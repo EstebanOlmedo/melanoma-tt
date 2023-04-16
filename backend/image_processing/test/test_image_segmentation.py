2b0486f12bc0a949702714a120bb420d739ab1b3
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2023-04-16 17:00:20 -0600

    Fix lint errors

--- backend/image_processing/test/test_image_segmentation.py
+++ backend/image_processing/test/test_image_segmentation.py
@@ -4,6 +4,7 @@ from ..testing import get_f1_score, get_mssism
 from ..noise_removal import hair_removal, lession_segmentation
 from ..image import ImageMetadata, TEST_IMAGES
 
+
 @pytest.mark.parametrize('image_metadata', TEST_IMAGES)
 def test_image_modification_in_hair_removal(image_metadata: ImageMetadata):
     approved_percentage = 0.8
@@ -15,6 +16,7 @@ def test_image_modification_in_hair_removal(image_metadata: ImageMetadata):
     mssimv = get_mssism(img_org, img)
     for i in range(3):
         assert mssimv[i] >= approved_percentage
+
 
 @pytest.mark.parametrize('image_metadata', TEST_IMAGES)
 def test_image_segmentation_precision(image_metadata: ImageMetadata):
