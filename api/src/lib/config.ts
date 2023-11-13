31688e9d3514f48a0fa81c70a7bab50a5cb93a19
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2023-11-13 14:13:14 -0600

    Track common.yml

--- api/src/lib/config.ts
+++ api/src/lib/config.ts
@@ -1,7 +1,6 @@
 import * as path from 'path';
 import { load } from 'node-yaml-config';
 import type IConfig from '../config';
-import 'dotenv/config';
 
 const config = load(
   path.join(__dirname, '../../config/common.yml'),
