41913aa7b5633d062862fe64894a8579303911c7
DanielMontesGuerrero <danimontesguerrero17@gmail.com>
2023-11-17 19:28:09 -0600

    Skip login when user cached

--- melanoma_app/app/login/index.tsx
+++ melanoma_app/app/login/index.tsx
@@ -1,14 +1,21 @@
+import { Redirect } from "expo-router";
 import { StyleSheet, View } from "react-native";
 
 import ColorPallete from "@/colorPallete";
 import LoginMenu from "@/components/login/loginForm";
+import { useUser } from "@/contexts/userContext";
 
 const Login = () => {
-  return (
-    <View style={styles.container}>
-      <LoginMenu />
-    </View>
-  );
+  const { user } = useUser();
+
+  if (user?.id === undefined) {
+    return (
+      <View style={styles.container}>
+        <LoginMenu />
+      </View>
+    );
+  }
+  return <Redirect href="/(home)/followup" />;
 };
 
 const styles = StyleSheet.create({
