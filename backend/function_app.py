716a880195613275ec93098ec098e0ac0e80d7fe
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2023-11-23 23:35:03 -0600

    Add try except to avoid crashing the application

--- backend/function_app.py
+++ backend/function_app.py
@@ -8,17 +8,25 @@ import dispatcher
 app = func.FunctionApp()
 
 @app.function_name(name="operation")
-@app.route(route="analyze")
+@app.route(route="analyze", methods=['GET', 'POST'])
 def analyze(req: func.HttpRequest) -> func.HttpResponse:
-    body = req.get_json()
-    if 'blobNames' not in body:
-        return func.HttpResponse("bad request", status_code=400)
-    if 'op' not in body:
-        return func.HttpResponse("bad request", status_code=400)
+    try:
+        body = req.get_json()
+        logging.info('body: ', body)
+        if 'blobNames' not in body:
+            return func.HttpResponse("bad request", status_code=400)
+        if 'op' not in body:
+            return func.HttpResponse("bad request", status_code=400)
 
-    result = dispatcher.dispatch(body['op'], body['blobNames'])
-    return func.HttpResponse(
-            json.dumps(result['data']),
-            mimetype="application/json",
-            status_code=result['status']
-    )
+        result = dispatcher.dispatch(body['op'], body['blobNames'])
+        return func.HttpResponse(
+                json.dumps(result['data']),
+                mimetype="application/json",
+                status_code=result['status']
+        )
+    except:
+        return func.HttpResponse(
+                'internal error',
+                mimetype="application/json",
+                status_code=500
+        )
