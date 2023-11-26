f7c86e2850cce2bc3584a7f4535ee637ab7b8bf8
EstebanOlmedo <esteban.olmedo.ramirez@gmail.com>
2023-11-25 18:32:29 -0600

    Change operation endpoint to call azure function

--- api/src/api/services/operation.ts
+++ api/src/api/services/operation.ts
@@ -1,82 +1,46 @@
 import { type RequestOptions } from '../../lib/types';
-import * as path from 'path';
-import { spawn } from 'child_process';
 import log from '../../lib/logger';
+import axios, { type AxiosResponse } from 'axios';
 
-interface ProcessData {
-  result: boolean;
-  message: string;
+interface Operation {
+  op: string;
+  blobNames: string[];
 }
 
-interface IProcessResult {
-  status: number;
-  data: ProcessData | string;
-}
-
-export const spawnProcess = async (
+export const makeHttpRequest = async (
   options: RequestOptions<
   unknown,
   { cmd: { cmd: string; blobNameBefore: string; blobNameAfter: string } }
   >,
 ) => {
-  const cmd = options.params.cmd;
-  const pythonProcess = spawn('python3', [
-    path.join(__dirname, '../../../../backend/dispatcher.py'),
-    cmd.cmd,
-    cmd.blobNameBefore,
-    cmd.blobNameAfter,
-  ]);
-
-  let dataRes = '';
-  let errorRes = '';
-
-  const handleProcessCompletion = async () => {
-    if (errorRes.length) {
-      log.info(`stderr data: ${errorRes}`);
-      return await Promise.resolve({
-        status: 500,
-        data: {
-          result: false,
-          message: errorRes,
+  try {
+    const azFunc = 'http://localhost:7071/api/analyze';
+    const cmd = options.params.cmd;
+    const { data, status }: AxiosResponse<Operation> = await axios.post(
+      azFunc,
+      {
+        op: cmd.cmd,
+        blobNames: [cmd.blobNameBefore, cmd.blobNameAfter],
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
         },
-      });
-    }
-
-    log.info(`stdout data: ${dataRes}`);
-    return await Promise.resolve({
-      status: 200,
-      data: dataRes,
-    });
-  };
-
-  // Attach event handlers
-  pythonProcess.stdout.on('data', (data) => {
-    const parsedData = JSON.parse(data);
-    dataRes += JSON.stringify(parsedData);
-  });
-  pythonProcess.stderr.on('data', (data) => {
-    const parsedData = JSON.parse(data);
-    errorRes += JSON.stringify(parsedData);
-  });
-
-  // Wrap the process completion logic in a promise
-  const processCompletionPromise = new Promise<PromiseLike<IProcessResult>>(
-    (resolve, reject) => {
-      pythonProcess.on('close', () => {
-        const result = handleProcessCompletion();
-        resolve(result);
-      });
-
-      // Handle process errors
-      pythonProcess.on('error', (err) => {
-        reject(err);
-      });
-    },
-  );
-<<<<<<< HEAD
-
-=======
->>>>>>> 93785a3 (Revert "Erase operation endpoint from api code")
-  // Wait for the process to complete and return the result
-  return await processCompletionPromise;
+      },
+    );
+    return {
+      status,
+      data,
+    };
+  } catch (error) {
+    log.error('Error:', error);
+    return {
+      status: 500,
+      data: {
+        result: false,
+        message: error,
+      },
+    };
+  }
 };
